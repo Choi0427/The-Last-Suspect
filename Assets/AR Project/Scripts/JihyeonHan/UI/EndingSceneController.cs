009b8253fc891bd2089b7c87819ae6f8100152db
jesta27 <143687515+jesta27@users.noreply.github.com>
2026-04-20 18:28:48 +0900

    UI/사운드 개발 완료 3
    스크립트 수정

--- Assets/AR Project/Scripts/JihyeonHan/UI/EndingSceneController.cs
+++ Assets/AR Project/Scripts/JihyeonHan/UI/EndingSceneController.cs
@@ -13,9 +13,9 @@ public class EndingSceneController : MonoBehaviour
 
     [Header("Messages")]
     [TextArea(3, 8)]
-    [SerializeField] private string trueEndingMessage = "TRUE ENDING\n\n선임 형사의 반응이 어딘가 수상했습니다. 끝까지 의심한 끝에, 당신은 사건의 진실에 도달했습니다.";
+    [SerializeField] private string trueEndingMessage = "TRUE ENDING";
     [TextArea(3, 8)]
-    [SerializeField] private string badEndingMessage = "BAD ENDING\n\n선택은 틀렸습니다. 진범은 아직 당신을 지켜보고 있습니다. 증거를 다시 검토해보세요.";
+    [SerializeField] private string badEndingMessage = "BAD ENDING";
 
     private void Start()
     {
